0bf6e01732f023f7bb679e7a151160cac22493ce
Kulev Anatoly <kulevw@gmail.com>
2026-06-19 23:40:16 +0500

    rename repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/kulevw.github.io/',
+  base: '/portfolio/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
